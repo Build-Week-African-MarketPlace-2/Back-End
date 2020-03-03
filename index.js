4845fd44addd14b3173e7bd040e7e3787080d107
Andrea Harris <Andrea@zacs-imac.lan>
2020-03-03 11:29:32 -0600

    debugging

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const server = require('./api/server')
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
 server.listen(port, () => {
-    console.log(`Server Listening On Port ${port}`)
+    console.log(`Server Listening On Port ${port}...`)
 })
